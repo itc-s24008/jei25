c0aea1dfe26ceed72aacdea8328ca9fb309eebb3
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-16 11:39:11 +0900

    課題

---
+++ D48Q17.java
@@ -0,0 +1,13 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        int n = 35;
+        System.out.println(n > 15 && n <= 30);
+        System.out.println(n > 30 && n <= 40);
+    }
+}
+
+
+/*
+D48Q17について
+D48は第48回のことを表しています
+ */
