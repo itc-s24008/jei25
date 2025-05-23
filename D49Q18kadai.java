c942440654983378c04578f325e49c5ccd6b67a6
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-23 11:15:51 +0900

    課題

---
+++ D49Q18kadai.java
@@ -0,0 +1,10 @@
+public class D49Q18kadai {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 1;
+        int c = 10;
+        System.out.println((double) a / b * c);
+    }
+}
+
+
