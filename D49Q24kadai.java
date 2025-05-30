c2a58d8548223182f6dcf3690a082a5018ba8aab
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-30 10:03:47 +0900

    課題

---
+++ D49Q24kadai.java
@@ -0,0 +1,16 @@
+public class D49Q24kadai {
+    public class Main {
+        public static void main(String[] args) {
+            int[] values = {10, 20, 30, 40};  // 合計: 100、要素数: 4
+
+            int sum = 0;
+            for (int value : values) {
+                sum += value;
+            }
+
+            System.out.println(sum);
+            System.out.println(values.length);
+        }
+
+    }
+}
