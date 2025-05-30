ca56e95310edd777be93e696b71e7482087e1502
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-30 12:35:12 +0900

    課題

--- D50Q25kadai.java
+++ D50Q25kadai.java
@@ -2,7 +2,7 @@ public class D50Q25kadai {
     public static void main(String[] args) {
         for (int i = 0; i <= 8; i++) {
             if (i % 2 != 0)
-                continue; 
+                continue;
             System.out.print(i);
         }
     }
