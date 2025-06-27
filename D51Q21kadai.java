af07536d48a2bafa654b74a92e670eeab973c34d
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-06-27 12:15:57 +0900

    課題

--- D51Q21kadai.java
+++ D51Q21kadai.java
@@ -1,6 +1,14 @@
 public class D51Q21kadai {
     public static void main(String[] args) {
-        int a = 021, b = 0x21;
-        System.out.println(a + b);
+        int a = 20;      // 10進数（そのまま20）
+        int b = 025;       // 8進数 2×8 + 5 = 21
+        int c = 0x10;        // 16進数 1×16 = 16
+        int d = 0b11;     // 2進数 1×2 + 1 = 3
+
+        // 各基数の値を合計
+        int total = a + b + c + d;
+
+        // 結果の表示
+        System.out.println("合計 = " + total);
     }
 }
--- D51Q22.java
+++ D51Q22.java
@@ -1,14 +1,8 @@
 public class D51Q22 {
     public static void main(String[] args) {
-        int a = 0b11; // 2進数の3
-        int b = 5;   // 10進数の5
-        int sum = a + b; // 3 + 5 = 8
-
-        // 計算結果を表示
-        view(true, "計算結果（0b11 + 5）は " + sum);
+        view(false, "true");
     }
-
     public static void view(boolean b, String s) {
-        System.out.println(b + " " + s);
+        System.out.println(" " + b + s);
     }
 }
